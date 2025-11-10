2c8419ffea3ca8a8d397386b5f73c5c1c3e934f4
gbsxl <gasousaoli@gmail.com>
2025-11-10 12:13:13 -0300

    init: first commit

---
+++ src/main/java/amen/and/Confide/ConfideApplication.java
@@ -0,0 +1,13 @@
+package amen.and.Confide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfideApplication.class, args);
+	}
+
+}
---
+++ src/test/java/amen/and/Confide/ConfideApplicationTests.java
@@ -0,0 +1,13 @@
+package amen.and.Confide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
