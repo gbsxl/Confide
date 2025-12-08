47b29749b250c71703337af6406e12603395b187
gbsxl <gasousaoli@gmail.com>
2025-12-08 11:35:11 -0300

    chore: Update frontend and backend dependencies.

--- backend/src/main/java/amen/and/Confide/configuration/CorsConfig.java
+++ backend/src/main/java/amen/and/Confide/configuration/CorsConfig.java
@@ -14,10 +14,10 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("http://127.0.0.1:5173")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedMethods("POST")
                         .allowCredentials(true);
-
             }
+            UsernameNotFoundException()
         };
     }
 }
